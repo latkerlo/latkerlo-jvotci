c6f588b112d15ba5537179e5a7c7d163f0e20519
berrymot <merrybot47@gmail.com>
2023-12-31 16:55:11 -0800

    stringify veljvo ex

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,6 +1,6 @@
 use latkerlo_jvotci::*;
 
 fn main() {
-    println!("{:?}", katna::get_veljvo("jvoco'e"));
+    println!("{}", katna::get_veljvo("jvoco'e").join(" "));
     println!("{}", jvozba::get_lujvo("lujvo co'e", true).unwrap().0);
 }
