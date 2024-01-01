deebd5591515cff2c5c4e45c0323e03fc0f8b633
berrymot <merrybot47@gmail.com>
2024-01-01 14:00:27 -0800

    change examples

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -2,6 +2,6 @@ use latkerlo_jvotci::*;
 
 fn main() {
 
-    println!("{:?}", katna::get_veljvo("jvoco'e").join(" "));
-    println!("{:?}", jvozba::get_lujvo("jvo- co'e-", true).unwrap().0); // panics ugh
+    println!("{}", katna::get_veljvo("jvoka'a").join(" "));
+    println!("{}", jvozba::get_lujvo("lujvo zbasu", false).unwrap().0);
 }
