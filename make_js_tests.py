ae71c13e5496092c39952d4715aac65204056ac5
berrymot <merrybot47@gmail.com>
2024-04-06 10:54:25 -0700

    add score tests

--- make_js_tests.py
+++ make_js_tests.py
@@ -27,7 +27,7 @@ with open("js/docs/jvozba_test_list.js", "w", encoding="utf-8") as opf:
             for test in test_list:
                 opf.write(f"  {test},\n")
         opf.write("]\n\n")
-TEST_LISTS.pop("LUJVO_SCORE_TESTS")
+# TEST_LISTS.pop("LUJVO_SCORE_TESTS")
 with open("rs/src/test_list.rs", "w", encoding="utf-8") as opf:
     opf.write("#[cfg(test)]\nuse crate::*;\n\n")
     for list_name, test_list in TEST_LISTS.items():
@@ -36,21 +36,24 @@ with open("rs/src/test_list.rs", "w", encoding="utf-8") as opf:
         opf.write("#[test]\n" + ("#[should_panic]\n" if "F" in list_name else "") + f"fn {list_name.lower()}() {{\n")
         opf.write("    let tests = [\n")
         for test in test_list:
-            opf.write(f"        [\"{test[0]}\"" + (f", \"{test[1]}\"" if "F" not in list_name else "") + "],\n")
+            opf.write(f"        (\"{test[0]}\"" + (f", {test[1]}" if "R" in list_name else f", \"{test[1]}\"" if "F" not in list_name else "") + "),\n")
         opf.write("    ];\n")
         opf.write("    for test in tests {\n")
         if "FAIL" in list_name:
-            opf.write("        println!(\"{}\", test[0]);\n")
+            opf.write("        println!(\"{}\", test);\n")
             if "Z" in list_name:
-                opf.write(f"        let _ = jvozba::get_lujvo(test[0], false);\n")
+                opf.write(f"        jvozba::get_lujvo(test, false).unwrap();\n")
             if "K" in list_name:
-                opf.write(f"        let _ = katna::get_veljvo(test[0]);\n")
+                opf.write(f"        katna::get_veljvo(test).unwrap();\n")
         else:
-            opf.write("        println!(\"{} / {}\", test[0], test[1]);\n")
+            opf.write("        println!(\"{} / {}\", test.0, test.1);\n")
             if "Z" in list_name or "M" in list_name:
-                opf.write("        assert_eq!(test[0], jvozba::get_lujvo(test[1], " + str("C" in list_name).lower() + ").unwrap().0);\n")
+                opf.write("        assert_eq!(test.0, jvozba::get_lujvo(test.1, " + str("M" in list_name).lower() + ").unwrap().0);\n")
                 opf.write("        println!(\"zbasu: pass\");\n")
             if "K" in list_name or "M" in list_name:
-                opf.write("        assert_eq!(katna::get_veljvo(test[0]).unwrap().join(\" \"), test[1]);\n")
+                opf.write("        assert_eq!(katna::get_veljvo(test.0).unwrap().join(\" \"), test.1);\n")
                 opf.write("        println!(\"katna: pass\");\n")
-        opf.write("    }\n}\n\n")
+            if "R" in list_name:
+                opf.write("        assert_eq!(jvozba::get_lujvo(test.0, false).unwrap().1, test.1);\n")
+                opf.write("        println!(\"score: pass\");\n")
+        opf.write("    }\n}\n")
