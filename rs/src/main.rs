b832ef19d84939e29a95dcc0acfe2b1337a2d32b
berrymot <merrybot47@gmail.com>
2024-06-08 22:08:59 -0700

    demo problems

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,9 +1,28 @@
+use humantime::format_duration;
 use latkerlo_jvotci::*;
-use tarmi::rafsi_tarmi;
-// use rafsi::RAFSI;
+use std::{collections::HashMap, time::Instant};
 
 fn main() {
     let settings = &Settings::default();
-    println!("{:?}", rafsi_tarmi("lo'"));
-    println!("{:?}", get_lujvo("djedi", settings));
+    let mut the = HashMap::new();
+    let start = Instant::now();
+    const END: u32 = 10000; // takes ~10m with `--release`
+    for i in 0..END {
+        println!(
+            "{i}    {}",
+            format_duration(start.elapsed() / if i != 0 { i } else { 1 } * (END - i))
+        );
+        let k = get_lujvo("condi djedi", settings).unwrap();
+        if the.contains_key(&k) {
+            the.insert(k.clone(), the.get(&k).unwrap() + 1);
+        } else {
+            the.insert(k, 1);
+        }
+    }
+    println!("{the:?}");
+    println!("{:?}", get_veljvo("jvo'ytci", settings)); // bad
+    println!("{:?}", get_veljvo("lujvo'ytci", settings)); // ok
+    println!("{:?}", get_veljvo("uajvo'ytci", settings)); // ok
+    println!("{:?}", get_veljvo("latratyraty'ismu", settings)); // bad, la falls off
+    println!("{:?}", get_veljvo("tratraty'ismu", settings)); // ok
 }
