3eec495fbd566a23adc92245d8b81a09801a0c01
berrymot <merrybot47@gmail.com>
2024-11-18 20:00:46 -0800

    [main.rs] remove inaccurate comment & unnecessary writing to file

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,12 +1,12 @@
 use humantime::format_duration;
 use latkerlo_jvotci::*;
-use std::{collections::HashMap, fs, time::Instant};
+use std::{collections::HashMap, time::Instant};
 
 fn main() {
     let settings = &Settings::default();
     let mut the = HashMap::new();
     let start = Instant::now();
-    const END: u32 = 100000; // takes ~10m with `--release`
+    const END: u32 = 100000;
     for i in 0..END {
         let eta = if i != 0 {
             &format_duration(start.elapsed() / i * (END - i)).to_string()
@@ -14,7 +14,6 @@ fn main() {
             ""
         };
         println!("{i}    {}", eta);
-        fs::write("eta.txt", eta).unwrap();
         let k = get_lujvo("condi djedi", settings).unwrap();
         if the.contains_key(&k) {
             the.insert(k.clone(), the.get(&k).unwrap() + 1);
